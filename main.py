2b1a317f7a0ec4e3fa63b90f7f6082dddd01e44b
TaeSungKim <ssuzi58@gmail.com>
2023-03-21 13:14:25 +0900

    Master1 입출력 기능들을 완성함.

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+from open1 import gray # open1.py 파일의 gray() 함수를가져옴
+from open2 import hsvm binary #open2.py 파일의 hsv(), binary()함수를 가져옴/
+# 영상 소스 열기
+src = cv2.imread('c:/photo/picture99.jpg')
+dst1= gray(src)
+dst2= hsv(src)
+dst3= binary(src)
+
+
+# 영상 디스플레이
+cv2.imshow('src', src)
+cv2.imshow('dst1', dst1)
+cv2.imshow('dst2', dst2)
+cv2.imshow('dst3', dst3)
+
+# 마무리
+cv2.waitKey(0)
+cv2.destroyAllWindows()
