a7d5a36fd6b936e2e9d0f0b0b47d498cb110eb49
TaeSungKim <ssuzi58@gmail.com>
2023-03-21 13:19:02 +0900

    Foo 2

---
+++ open1.py
@@ -0,0 +1,10 @@
+import cv2
+
+def hsv(src) :
+    dest = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    return dest
+
+def binary(src) :
+    dest = src.copy()
+    _, dest = cv2.threshold(dst3, 127, 255, cv2.THRESH_BINARY)
+    return dest
