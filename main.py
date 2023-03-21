4a495d4eba1a1b3e6a4b0e3566bd6b34f91b0069
TaeSungKim <ssuzi58@gmail.com>
2023-03-21 13:23:42 +0900

    Master 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
-from open1 import gray # open1.py 파일의 gray() 함수를가져옴
-from open2 import hsvm binary #open2.py 파일의 hsv(), binary()함수를 가져옴/
+from open2 import gray # open1.py 파일의 gray() 함수를가져옴
+from open1 import hsv binary #open2.py 파일의 hsv(), binary()함수를 가져옴/
 # 영상 소스 열기
 src = cv2.imread('c:/photo/picture99.jpg')
 dst1= gray(src)
